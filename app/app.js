350c3bb5634ed1bed4653c6ad62c8a1b2c002401
Jiayi Zheng <thebluber@gmail.com>
2016-08-21 15:23:04 +0200

    Use bootstrap-css, angular-ui-bootstrap, font-awesome Add some html skelettons for the landing page Add css loader and font loaders in webpack config

--- app/app.js
+++ app/app.js
@@ -1,11 +1,16 @@
-require('file?name=[name].[ext]!./index.html')
-require('css!less!spectre.css')
+require('file?name=[name].[ext]!./index.html');
+require('expose?$!expose?jQuery!jquery');
+require('./css/custom.css');
+require('bootstrap-css');
+require('./js/custom.js');
+require('./css/font-awesome-4.6.3/css/font-awesome.min.css');
 
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
+import tooltip from 'angular-ui-bootstrap/src/tooltip';
 
 import test from './directives/test.js';
 
-angular.module('theWiredFE', []).
+angular.module('theWiredFE', [tooltip]).
   directive('test', test);
 
--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,12 @@ module.exports = {
           presets: ['es2015']
         },
         exclude: /node_modules/
-      }
+      },
+      { test: /\.css$/, loader: "style-loader!css-loader" },
+      { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=application/octet-stream" },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=image/svg+xml" }
     ]
   }
 };
